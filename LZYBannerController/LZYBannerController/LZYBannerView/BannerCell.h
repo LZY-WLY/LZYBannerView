4c639125b76ba99ce11490478fd43ceeae3b0f79
lzy <18860233120@163.com>
2018-05-20 11:54:32 +0800

    第一版本

---
+++ LZYBannerController/LZYBannerController/LZYBannerView/BannerCell.h
@@ -0,0 +1,14 @@
+//
+//  BannerCell.h
+//  YFiOS
+//
+//  Created by yfmac001 on 2018/4/11.
+//  Copyright © 2018年 yfmac001. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BannerCell : UICollectionViewCell
+///轮播图 显示的地址
+@property (nonatomic, copy) NSString *bannerUrl;
+@end
